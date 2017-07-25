9489375f32299440fac24178c6b07012108cf679
Mario Martinez <mario.martinez@masclicks.com>
2017-07-25 11:55:27 -0500

    calendario

--- calendario.php
+++ calendario.php
@@ -1,6 +1,6 @@
 <?php include_once 'includes/templates/header.php'; ?>
 
-    <section class="seccion contenedor calendario">
+    <section class="seccion contenedor" id="schedule">
         <h2>Calendario de eventos</h2>
     
         <?php
@@ -21,49 +21,80 @@
             }
         ?>
 
-        <?php while($eventos = $resultado->fetch_all(MYSQLI_ASSOC)) { ?>
+        <div class="calendario">
+            <?php while($eventos = $resultado->fetch_all(MYSQLI_ASSOC)) { ?>
+           
 
-            <?php foreach($eventos as $evento): ?>
-                <div class="dia">
-                    <p class="titulo"><?php echo utf8_encode($evento['nombre_evento']); ?></p>
-                    <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> <?php echo $evento['fecha_evento']. " " . $evento['hora_evento'] . " hrs" ; ?></p>
-                    <p>
-                        <?php $categoria_evento = $evento['cat_evento']; ?>                    
+                <?php $dias = array(); ?>
+                <?php foreach($eventos as $evento) {
+                    $dias[] = $evento['fecha_evento'];
+                } ?>
 
-                        <?php
-                            switch($categoria_evento){
-                                case 'Talleres':
-                                    echo '<i class="fa fa-code" aria-hidden"true"></i> Taller';
-                                    break;echo '';
-                                    break;
-                                case 'Conferencias':
-                                    echo '<i class="fa fa-comment" aria-hidden"true"></i> Conferencias';
-                                    break;
-                                case 'Seminario':
-                                    echo '<i class="fa fa-university" aria-hidden"true"></i> Seminario';
-                                    break;
-                                default:
-                                    echo '';
-                                    break;
-                            }
-                        ?>                    
-                    </p>
-                    <p> <i class="fa fa-user" aria-hidden="true"></i>
-                        <?php echo $evento['nombre_invitado'] . " " . $evento['apellido_invitado']; ?> 
-                    </p>
+                <?php $dias = array_values(array_unique($dias)); ?>
+                 
+                 
+                <?php $contador = 0; ?>
+                
+                <?php foreach($eventos as $evento): ?>
+                    
+                    <?php $dia_actual = $evento['fecha_evento']; ?>
+
+                    <?php if($dia_actual == $dias[$contador]): ?>
+                    
+                        <h3>
+                            <i class="fa fa-calendar" aria-hidden="true"></i>
+                            <?php echo $evento['fecha_evento']; ?>
+                        </h3>
+                        <?php $contador++; ?>
+                        <pre>
+                            <?php 
+                                var_dump($contador);
+                            ?>
+                        </pre>
+                        
+                        
+                    <?php endif; ?>
+
+                    <div class="dia">
+                        <p class="titulo"><?php echo utf8_encode($evento['nombre_evento']); ?></p>
+                        <p class="hora"><i class="fa fa-clock-o" aria-hidden="true"></i> <?php echo $evento['fecha_evento']. " " . $evento['hora_evento'] . " hrs" ; ?></p>
+                        <p>
+                            <?php $categoria_evento = $evento['cat_evento']; ?>                    
+
+                            <?php
+                                switch($categoria_evento){
+                                    case 'Talleres':
+                                        echo '<i class="fa fa-code" aria-hidden"true"></i> Taller';
+                                        break;echo '';
+                                        break;
+                                    case 'Conferencias':
+                                        echo '<i class="fa fa-comment" aria-hidden"true"></i> Conferencias';
+                                        break;
+                                    case 'Seminario':
+                                        echo '<i class="fa fa-university" aria-hidden"true"></i> Seminario';
+                                        break;
+                                    default:
+                                        echo '';
+                                        break;
+                                }
+                            ?>                    
+                        </p>
+                        <p> <i class="fa fa-user" aria-hidden="true"></i>
+                            <?php echo $evento['nombre_invitado'] . " " . $evento['apellido_invitado']; ?> 
+                        </p>
 
 
-                </div>
+                    </div>
 
-            <?php endforeach; ?>
+                <?php endforeach; ?>
 
+            </div>
 
-            <pre>
-                <?php var_dump($eventos);?>
-            </pre>
-
-
-        <?php } ?>
+            <?php } ?>
+            
+        <?php 
+           $conn->close();
+        ?>
 
     </section>
 
